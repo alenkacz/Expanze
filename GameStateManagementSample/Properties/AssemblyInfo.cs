0882575e35521f7f66f69bc68429418b61b9e9df
Lukas Beran <beranlukas@gmail.com>
2010-10-02 20:43:16 +0200

    Window title renamed

--- GameStateManagementSample/Properties/AssemblyInfo.cs
+++ GameStateManagementSample/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameStateManagementSample")]
-[assembly: AssemblyProduct("GameStateManagementSample")]
+[assembly: AssemblyTitle("Expanze")]
+[assembly: AssemblyProduct("Expanze")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
