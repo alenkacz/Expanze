e4d781bc4b3a66045a04cad32b69b1aba061d605
Lukas Beran <beranlukas@gmail.com>
2010-10-10 11:50:42 +0200

    Hex map 0.1

--- app/Expanze/Hexa.cs
+++ app/Expanze/Hexa.cs
@@ -12,9 +12,10 @@ namespace Expanze
 {
     class Hexa : DrawableGameComponent
     {
-        Model myModel;
+        const int N_MODEL = 7;
+        Model[] hexaModel;
+        Matrix[] world;
 
-        public Matrix world;
         public Matrix view;
         public Matrix projection;
         public Vector3 eye, target, up;
@@ -39,43 +40,70 @@ namespace Expanze
             up = new Vector3(0.0f, 1.0f, 0.0f);
             angle = 0.0f;
             view = Matrix.CreateLookAt(eye, target, up);
-            world = Matrix.Identity;
+
+            world = new Matrix[N_MODEL];
+            world[0] = Matrix.Identity;
+            Vector3 t = new Vector3(0.51f, 0.0f, 0.28f);
+            world[1] = Matrix.CreateTranslation(t);
+            t = new Vector3(-0.51f, 0.0f, 0.28f);
+            world[2] = Matrix.CreateTranslation(t);
+            t = new Vector3(0.51f, 0.0f, -0.28f);
+            world[3] = Matrix.CreateTranslation(t);
+            t = new Vector3(-0.51f, 0.0f, -0.28f);
+            world[4] = Matrix.CreateTranslation(t);
+            t = new Vector3(0.0f, 0.0f, -0.55f);
+            world[5] = Matrix.CreateTranslation(t);
+            t = new Vector3(0.0f, 0.0f, 0.55f);
+            world[6] = Matrix.CreateTranslation(t);
             projection = Matrix.CreatePerspectiveFieldOfView((float) MathHelper.ToRadians(90), aspectRatio, 1.0f, 100.0f);
         }
 
         protected override void LoadContent()
         {
-            myModel = Game.Content.Load<Model>("Models/redhex");
-            Matrix[] transforms = new Matrix[myModel.Bones.Count];
-            myModel.CopyAbsoluteBoneTransformsTo(transforms);
+            hexaModel = new Model[N_MODEL];
+            hexaModel[0] = Game.Content.Load<Model>("Models/redhex");
+            hexaModel[1] = Game.Content.Load<Model>("Models/greenhex");
+            hexaModel[2] = Game.Content.Load<Model>("Models/whitehex");
+            hexaModel[3] = Game.Content.Load<Model>("Models/brownhex");
+            hexaModel[4] = Game.Content.Load<Model>("Models/bluehex");
+            hexaModel[5] = Game.Content.Load<Model>("Models/bluehex");
+            hexaModel[6] = Game.Content.Load<Model>("Models/bluehex");
+            foreach (Model m in hexaModel)
+            {
+                Matrix[] transforms = new Matrix[m.Bones.Count];
+                m.CopyAbsoluteBoneTransformsTo(transforms);
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            angle = angle + gameTime.ElapsedGameTime.Milliseconds / 1000.0f * 1.0f;
+            if (angle > 360.0f)
+                angle -= 360.0f;
+            eye = new Vector3((float)(1.5f * Math.Cos(angle)), 2.0f, 1.5f * (float)Math.Sin(angle));
         }
 
         public override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
-            angle = angle + gameTime.ElapsedGameTime.Milliseconds / 1000.0f * 3.0f;
-            if (angle > 360.0f)
-                angle -= 360.0f;
-            eye = new Vector3((float) (5.0f * Math.Cos(angle)), 0.0f, 5.0f * (float) Math.Sin(angle));
+            
             view = Matrix.CreateLookAt(eye, target, up);
-            foreach (ModelMesh mesh in myModel.Meshes)
+            for (int loop1 = 0; loop1 < N_MODEL; loop1++)
             {
-                foreach (BasicEffect effect in mesh.Effects)
+                foreach (ModelMesh mesh in hexaModel[loop1].Meshes)
                 {
-                    effect.EnableDefaultLighting();
-                    effect.World = world;
-                    effect.View = view;
-                    effect.Projection = projection;
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.EnableDefaultLighting();
+                        effect.World = world[loop1];
+                        effect.View = view;
+                        effect.Projection = projection;
+                    }
+
+                    mesh.Draw();
                 }
-
-                mesh.Draw();
             }
-
         }
     }
     
