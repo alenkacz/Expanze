2ea8aef07959e07afbf7dc773ab1dc09e2227baa
Lukas Beran <beranlukas@gmail.com>
2010-11-05 16:30:37 +0100

    PickVariables.cs

---
+++ src/Expanze/MapGeneration/Hexas/PickVariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Expanze
+{
+    class PickVariables
+    {
+        public Boolean wasClickAnywhereNow = false;
+        public Boolean wasClickAnywhereLast = false;
+        public Boolean pickActive = false;         // if mouse is over pickable area
+        public Boolean pickNewActive = false;      // if mouse is newly over pickable area
+        public Boolean pickNewPress = false;       // if mouse press on pickable area newly
+        public Boolean pickPress = false;          // if mouse press on pickable area
+        public Boolean pickNewRelease = false;     // if mouse is newly release above pickable area
+    }
+}
