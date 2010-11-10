82540b2ed72fec413483d26dedc46f6802a9997b
Lukas Beran <beranlukas@gmail.com>
2010-11-10 18:09:23 +0100

    ISourceAll.cs

---
+++ src/CorePlugin/Interfaces/ISourceAll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorePlugin
+{
+    public interface ISourceAll
+    {
+        int getWood();
+        int getOre();
+        int getMeat();
+        int getStone();
+        int getCorn();
+    }
+}
