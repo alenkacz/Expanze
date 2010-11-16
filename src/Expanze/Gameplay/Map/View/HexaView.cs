16b9997136f2c65c016a51eb06fc36d3230c04cd
Lukas Beran <beranlukas@gmail.com>
2010-11-16 16:25:47 +0100

    press to release (building in town)

--- src/Expanze/Gameplay/Map/View/HexaView.cs
+++ src/Expanze/Gameplay/Map/View/HexaView.cs
@@ -201,7 +201,7 @@ namespace Expanze
 
             Map.SetPickVariables(c == pickHexaColor, pickVars);
 
-            if (pickVars.pickNewPress)
+            if (pickVars.pickNewRelease)
             {
                 for (int loop1 = 0; loop1 < townView.Length; loop1++)
                 {
