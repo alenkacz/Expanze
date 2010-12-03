e342c3f528ad1259cd8bb8b9a153cadde3e1cfd2
alenkacz <varkockova.a@gmail.com>
2010-12-03 17:02:24 +0100

    different map types, added functionality to parser

--- src/Expanze/Gameplay/Map/MapGeneration/MapParser.cs
+++ src/Expanze/Gameplay/Map/MapGeneration/MapParser.cs
@@ -10,12 +10,18 @@ namespace Expanze
     class MapParser
     {
         XmlDocument xDoc;
+        System.Random generator = new System.Random();
 
         Dictionary<int,int> lowProductivityList = new Dictionary<int,int>();
         Dictionary<int, int> mediumProductivityList = new Dictionary<int, int>();
         Dictionary<int, int> highProductivityList = new Dictionary<int, int>();
 
+        Dictionary<string, int> normalMapTypes = new Dictionary<string, int>();
+        Dictionary<string, int> wastelandMapTypes = new Dictionary<string, int>();
+        Dictionary<string, int> lowlandMapTypes = new Dictionary<string, int>();
+
         Dictionary<int, int> activeProductivityList;
+        Dictionary<string, int> activeMapTypes;
 
         public MapParser()
         {
@@ -33,18 +39,19 @@ namespace Expanze
             HexaModel[][] map;
             // TODO should be done programatically
             activeProductivityList = highProductivityList;
+            activeMapTypes = lowlandMapTypes;
 
             //TODO user number parameter
             xDoc.Load("Content/Maps/" + "small" + ".xml");
 
             XmlNodeList productivities = xDoc.GetElementsByTagName("productivity");
+            XmlNodeList mapTypes = xDoc.GetElementsByTagName("mapType");
 
             this.parseProductivity(productivities);
+            this.parseMapTypes(mapTypes);
 
             XmlNodeList rows = xDoc.GetElementsByTagName("row");
             map = new HexaModel[rows.Count][];
-
-            String a = rows[1].InnerText;
 
             for (int i = 0; i < rows.Count; ++i)
             {
@@ -53,16 +60,28 @@ namespace Expanze
 
                 for (int j = 0; j < hexas.Count; ++j)
                 {
-                    XmlNode type = hexas[j].SelectSingleNode("type");
+                    bool isEmpty = false;
+
+                    foreach (XmlAttribute a in hexas[j].Attributes)
+                    {
+                        if(a.Name == "type" && a.Value == "space") isEmpty = true;
+                    }
+
+                    HexaKind type;
                     int hexanum = 0;
 
-                    if (decideType(type.InnerText) != HexaKind.Nothing)
+                    if (!isEmpty)
                     {
+                        type = getRandomType();
                         hexanum = getRandomProductivity();
+                    }
+                    else
+                    {
+                        type = HexaKind.Nothing;
                     }
 
 
-                    map[i][j] = HexaCreator.create(decideType(type.InnerText), hexanum);
+                    map[i][j] = HexaCreator.create(type, hexanum);
                 }
                 
             }
@@ -76,7 +95,6 @@ namespace Expanze
         /// <returns>value of productivity</returns>
         private int getRandomProductivity()
         {
-            System.Random generator = new System.Random();
             int index = 0;
             int type = 0;
 
@@ -105,12 +123,59 @@ namespace Expanze
         }
 
         /// <summary>
+        /// Returns randomly selected productivity value from loaded list
+        /// </summary>
+        /// <returns>value of productivity</returns>
+        private HexaKind getRandomType()
+        {
+            int index = 0;
+            string type = "";
+
+            if (!ranOutOfMapTypes())
+            {
+
+                while (true)
+                {
+                    // trying to find nonzero random type
+                    index = generator.Next(activeProductivityList.Count);
+
+                    if (activeMapTypes.ElementAt(index).Value > 0)
+                    {
+                        type = activeMapTypes.ElementAt(index).Key;
+                        break;
+                    }
+                }
+
+                // decrement number of available productivity numbers
+                --activeMapTypes[type];
+
+                return decideType(type);
+            }
+
+            return HexaKind.Nothing;
+        }
+
+        /// <summary>
         /// Checks whether there are some productivitiy points available - fix for indefinite loop, bad map design
         /// </summary>
         /// <returns>true if there are no productivity points left</returns>
-        private bool ranOutOfProductivities()
+        private bool ranOutOfProductivities( )
         {
             foreach (int i in activeProductivityList.Values)
+            {
+                if (i > 0) return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Checks whether there are some map type points available - fix for indefinite loop, bad map design
+        /// </summary>
+        /// <returns>true if there are no productivity points left</returns>
+        private bool ranOutOfMapTypes()
+        {
+            foreach (int i in activeMapTypes.Values)
             {
                 if (i > 0) return false;
             }
@@ -147,6 +212,35 @@ namespace Expanze
             }
         }
 
+        private void parseMapTypes(XmlNodeList xml)
+        {
+            foreach (XmlNode x in xml)
+            {
+                XmlAttributeCollection attCol = x.Attributes;
+                XmlNodeList items = x.ChildNodes;
+
+                foreach (XmlAttribute a in attCol)
+                {
+                    if (a.Value == "normal" && a.Name == "type")
+                    {
+                        fillMapTypesList(items, normalMapTypes);
+                    }
+                    else if (a.Value == "lowland" && a.Name == "type")
+                    {
+                        fillMapTypesList(items, lowlandMapTypes);
+                    }
+                    else if (a.Value == "wasteland" && a.Name == "type")
+                    {
+                        fillMapTypesList(items, wastelandMapTypes);
+                    }
+                    else
+                    {
+                        // TODO some exception should be triggered
+                    }
+                }
+            }
+        }
+
         private void fillProductivityList(XmlNodeList items, Dictionary<int,int> list)
         {
             foreach (XmlNode x in items)
@@ -163,6 +257,25 @@ namespace Expanze
                 }
 
                 list.Add(productivity, Int32.Parse(x.InnerText));
+            }
+        }
+
+        private void fillMapTypesList(XmlNodeList items, Dictionary<string, int> list)
+        {
+            foreach (XmlNode x in items)
+            {
+                XmlAttributeCollection attCol = x.Attributes;
+                string hexaType = "";
+
+                foreach (XmlAttribute a in attCol)
+                {
+                    if (a.Name == "type")
+                    {
+                        hexaType = a.Value;
+                    }
+                }
+
+                list.Add(hexaType, Int32.Parse(x.InnerText));
             }
         }
 
