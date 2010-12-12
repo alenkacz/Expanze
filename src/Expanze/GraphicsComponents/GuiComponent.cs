1fec46e68683a6c1b9226b34e58e4472b09fd753
alenkacz <varkockova.a@gmail.com>
2010-12-12 19:07:30 +0100

    graphics restructualisation

---
+++ src/Expanze/GraphicsComponents/GuiComponent.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace Expanze
+{
+    class GuiComponent : GameComponent
+    {
+        protected SpriteBatch spriteBatch;
+        public Texture2D myButton;
+        protected Vector2 spritePosition;
+        protected Game myGame;
+        protected SpriteFont gameFont;
+
+        protected int width;
+        protected int height;
+        protected String texture;
+        protected bool picked;
+
+        protected Boolean pick;
+
+        public GuiComponent() { }
+
+        public GuiComponent(Game game, int x, int y, SpriteFont font, int width, int height, String texture)
+        {
+            pick = false;
+
+            myGame = game;
+            spritePosition.X = x;
+            spritePosition.Y = y;
+            gameFont = font;
+            this.height = height;
+            this.width = width;
+            this.texture = texture;
+        }
+
+        public override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(myGame.GraphicsDevice);
+            if (texture != null)
+            {
+                myButton = myGame.Content.Load<Texture2D>(texture);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public void Draw(GameTime gameTime, Boolean pick)
+        {
+            this.pick = pick;
+            Draw(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            
+            Color c;
+            if (pick)
+            {
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, null, null, null, null, Settings.spriteScale);
+                c = Color.Black;
+            }
+            else
+            {
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, Settings.spriteScale);
+                c = Color.White;
+            }
+            if( myButton != null )
+                spriteBatch.Draw(myButton,spritePosition, c);
+
+            spriteBatch.End();
+        }
+
+        public void setPicked(Boolean picked)
+        {
+            this.picked = picked;
+        }
+
+        public bool getPicked()
+        {
+            return this.picked;
+        }
+
+        public void changePosition(Vector2 p) 
+        {
+            spritePosition = p;
+        }
+
+    }
+}
